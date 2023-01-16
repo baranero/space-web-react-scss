9dd725aa3cbdd4875504dc8df5f0fab2cb6c876f
baranero <jakub.baran2@gmail.com>
2023-01-16 12:11:17 +0100

    fix bug with background

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Fragment, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import './App.sass';
 import Destination from './components/Destination/Destination';
@@ -13,15 +13,32 @@ console.log(moonsData);
 
 function App() {
 
-  const [background, setBackground] = useState(homepage)
+  const [background, setBackground] = useState('')
+
+  useEffect(() => {
+    const data = localStorage.getItem('background')
+    if (data) {
+      setBackground(data) }
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem('background', background)
+  }, [background])
+
+
+
+  console.log(background);
+ 
+
+
 
   return (
 
       <BrowserRouter>
-        <div className='App' style={{ backgroundImage: `url(${background})` }} >
+        <div className='App' style={{ backgroundImage: background ? `url(${background})` : `url(${homepage})` }} >
           <Header onBackground={setBackground} destination={destination} homepage={homepage} />
           <Routes>``
-            <Route path='/' element={<HomePage/>}/>
+            <Route path='/' element={<HomePage onBackground={setBackground} destination={destination} homepage={homepage} />}/>
             <Route path='/destination' element={<Destination data={moonsData}/>}/>
           </Routes>
           </div>
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,7 +1,7 @@
 import './HomePage.sass'
 import { Fragment } from 'react'
 import explore from '../../images/oval.png'
-import Header from '../Header/Header'
+
 import { Link, useLocation } from 'react-router-dom'
 
 const HomePage = (props) => {
@@ -15,7 +15,7 @@ const HomePage = (props) => {
                     <h1>SPACE</h1>
                     <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
                 </div>
-                <Link to="/destination"><img src={explore} alt="Explore button" /></Link>
+                <Link onClick={() => props.onBackground(props.destination)} to="/destination"><img src={explore} alt="Explore button" /></Link>
             </section>
         </Fragment>
     )
